16e2320248ac05165ce3725b6cd15be6dd0ab78e
Jessie Han <79-hanj527@users.noreply.gitlab.newschool.edu>
2021-10-24 13:00:13 -0400

    upload

--- qualitative_data/src/main.js
+++ qualitative_data/src/main.js
@@ -2,6 +2,16 @@ var tilesPerRow = 5;
 var tileSize = 80;
 var barWidth = tilesPerRow * tileSize + 10;
 
+var filteredData; 
+var selectMode = "show", selectedYearBegin = "1894", selectedYearEnd = "1919";
+
+const COLOR = {
+  "USA": "#8CC6D4",
+  "Japan": "#BAABC8",
+  "Mexico": "#EAD971"
+}
+
+
 function getTiles(num) {
   var tiles = [];
 
@@ -15,6 +25,52 @@ function getTiles(num) {
 
   return tiles
 }
+
+function updateBarHide(d, i, items) {
+
+  console.log (d, i, items)
+
+  var tiles = getTiles(d);
+
+  var u = d3.select("#_" + i)
+    .attr("transform", "translate(" + i * barWidth + ", 900)")
+    .selectAll("rect")
+    .data(tiles);
+
+
+      // only color tiles------------------------------
+      u.enter()
+      .append("rect")
+      .style("stroke", "white")
+      .style("stroke-width", "0.5")
+      .style("shape-rendering", "crispEdges")
+
+    
+      .merge(u)
+      .attr("x", function(d) {       
+        return d.x;
+      })
+      .attr("y", function(d) {
+        return d.y;
+      })
+      .attr("width", tileSize)
+      .attr("height", tileSize)
+      .transition()
+      .delay(function(d, i) {
+        return i * 20;
+      })
+      .style("opacity", 1)
+      .style("fill", function(d, i) {
+        return items[i].color;
+      })
+       //end of color tiles
+
+  u.exit().remove();
+
+  updateLabel(i, items)
+
+}
+
 
 function updateBar(d, i, items) {
 
@@ -45,13 +101,10 @@ function updateBar(d, i, items) {
         return i * 20;
       })
       .style("opacity", 1)
-
       .attr("xlink:href", function(d,i){
         return `${items[i].root}`
       })
-      // .attr("xlink:href", `https://s3.amazonaws.com/assets.saam.media/files/styles/x_large/s3/files/images/1992/SAAM-1992.13.107_1.jpg`)
-      //end of customize
-
+       //end of customize
 
   u.exit().remove();
 
@@ -74,30 +127,43 @@ function updateLabel(i, items) {
       .style("font-size", "30px")
       .style("fill", "#777");
   }
- 
-  el.text(items[0].group);
+  
+  if (items[0]){
+    el.text(items[0].group);
+  }
+
 }
 
 
-function updateBars(group_items) {
+function updateBars(group_items, selectMode) {
 
   var u = d3.select("g.bars")
-    .selectAll("g")
-    .data(getGroupCounts(group_items));
-   
-  u.enter()
+    .selectAll("g").remove();
+
+    u = d3.select("g.bars")
+    .selectAll("g").data(getGroupCounts(group_items));
+  
+  if (selectMode == "show"){
+    u.enter()
     .append("g")
     .attr("id", function(d,i){ return "_" + i; } )
     .merge(u)
     .each(function(d,i){
-      updateBar(d, i, group_items[i])
-     
+      updateBar(d, i, group_items[i])  
     })
-   
-      
-      
-      
-   
+  }
+
+  else{
+    u.enter()
+    .append("g")
+    .attr("id", function(d,i){ return "_" + i; } )
+    .merge(u)
+    .each(function(d,i){
+      updateBarHide(d, i, group_items[i])  
+    })
+
+  }
+  
   u.exit().remove();
 }
 
@@ -112,39 +178,73 @@ function getGroupCounts(group_items){
   return counts
 }
 
+function prepareJson(data, selectedYearBegin, selectedYearEnd){
+
+    //re-arrange the data_json file to creat bar charts in d3
+    let order = ['Child', 'Mother(Adult)', 'Tool', 'Landscape', 'Animal', 'Plant']
+
+    let g = data.map( d => { return d.group})
+
+    let groups_raw = [...new Set(g)]
+
+    let groups = order.map( function(g){
+
+      console.log(g)
+      if (groups_raw.includes(g)){
+        return g
+      }
+    });
+
+    // groups = groups.slice().sort(function(a,b) {
+    //   return order.indexOf( a.group ) - order.indexOf( b.group );
+    // })
+
+    console.log(groups)
+
+    let group_counts = new Array(groups.length).fill(0);
+    let group_items = [];
+
+    for (let i=0; i< groups.length; i++){
+      group_items[i] = [];
+    }
+
+    for (let i=0; i< groups.length; i++){
+
+        for (let d in data){
+
+          if (data[d].group === groups[i] && data[d].date >= selectedYearBegin && data[d].date <= selectedYearEnd ){
+
+            group_counts[i] += 1;
+            group_items[i].push(data[d]);
+          }
+        }
+    }
+
+   return group_items
+
+}
 
 d3.json("./data_crop.json", function(err, data) {
   
- //re-arrange the data_json file to creat bar charts in d3
- let groups = ['Child', 'Figur', 'Tools', 'Lands', 'Animl', 'Plant']
+ 
+  let group_items = prepareJson(data, selectedYearBegin, selectedYearEnd);
 
- let group_counts = new Array(groups.length).fill(0);
- let group_items = [];
+  updateBars(group_items, selectMode);
 
- for (let i=0; i< groups.length; i++){
-   group_items[i] = [];
- }
+  d3.select("select.mode")
+  .on("change", function() {
+    selectMode = this.value;
+    updateBars(group_items, selectMode);
+  })
 
- for (let i=0; i< groups.length; i++){
-
-    for (let d in data){
-
-      if (data[d].group === groups[i]){
-
-        group_counts[i] += 1;
-        group_items[i].push(data[d]);
-      }
-    }
- }
-
-  console.log(group_items)
-
-  // initialize();
-  // update();
-
-  updateBars(group_items);
-
-
-  // updateBar(15, data.map( d => d.root));
+  d3.select("select.year")
+  .on("change", function() {
+    let value = this.value;
+    
+    selectedYearBegin = parseInt(value.split(',')[0])
+    selectedYearEnd = parseInt(value.split(',')[1])
+    let group_items = prepareJson(data, selectedYearBegin, selectedYearEnd);
+    updateBars(group_items, selectMode);
+  })
 
 });
